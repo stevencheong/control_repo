c0be0e838bed9f60d7b7ac2dd3de2a18de09adfa
stevencheong <74582451+stevencheong@users.noreply.github.com>
2020-11-26 11:21:59 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,17 @@
 node default { 
+  file { '/root/README':
+  content =>  'This is a readme',
+  owner   =>  'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+nodes /^web/ {
+  include role::app_server
+}
+
+nodes /^db/ {
+include role::db_server 
 }
